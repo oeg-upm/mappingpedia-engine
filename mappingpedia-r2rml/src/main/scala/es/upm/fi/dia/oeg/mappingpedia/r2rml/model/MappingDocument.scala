b9cc3b23bb1683c48c305c3f3ab8426cf56c7e5e
Freddy Priyatna <freddy.priyatna@gmail.com>
2017-04-07 14:38:48 +0200

    improve the endpoint for getting MappingDocuments

--- mappingpedia-r2rml/src/main/scala/es/upm/fi/dia/oeg/mappingpedia/r2rml/model/MappingDocument.scala
+++ mappingpedia-r2rml/src/main/scala/es/upm/fi/dia/oeg/mappingpedia/r2rml/model/MappingDocument.scala
@@ -3,10 +3,12 @@ package es.upm.fi.dia.oeg.mappingpedia.r2rml.model
 /**
   * Created by fpriyatna on 07/04/2017.
   */
-class MappingDocument(val id:String, val title:String, val dataset:String, val filePath:String) {
+class MappingDocument(val id:String, val title:String, val dataset:String, val filePath:String, val creator:String
+                      , val distribution:String, val distributionAccessURL:String) {
   def getId = this.id;
   def getTitle = this.title;
   def getDataset = this.dataset;
-  //def getFilePath = this.filePath;
-
+  def getCreator = this.creator;
+  //def getDistribution = this.distribution;
+  def getDistributionAccessURL = this.distributionAccessURL;
 }
