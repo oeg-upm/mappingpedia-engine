148d1b85bde6a50d1bc9b16d0149bf1b32ed19f6
Freddy Priyatna <fpriyatna@fi.upm.es>
2017-07-06 14:11:55 +0300

    getsubclasses endpoint to return superclasses

--- mappingpedia-r2rml/src/main/scala/es/upm/fi/dia/oeg/mappingpedia/OntologyClass.scala
+++ mappingpedia-r2rml/src/main/scala/es/upm/fi/dia/oeg/mappingpedia/OntologyClass.scala
@@ -1,12 +1,14 @@
 package es.upm.fi.dia.oeg.mappingpedia
 
+import es.upm.fi.dia.oeg.mappingpedia.r2rml.MappingPediaUtility
+
 /**
   * Created by fpriyatna on 6/7/17.
   */
 class OntologyClass (val aClass:String, val superClass:String, val superclasses:List[String]) {
   def getAClass = this.aClass;
-  def getSuperClass = this.superClass;
-  def getSuperclasses = this.superclasses;
+  //def getSuperClass = this.superClass;
+  def getSuperclasses = this.superclasses.mkString(",")
 
-  override def toString: String = this.aClass + " - " + this.superClass + " - " + this.superclasses.mkString;
+  override def toString: String = this.aClass + " - " + this.superclasses.mkString;
 }
